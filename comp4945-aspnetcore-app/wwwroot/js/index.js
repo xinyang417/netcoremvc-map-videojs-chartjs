531a76d0476f743e94d7d13415729be2dce065f8
Xinyang Li <xinyangli417@gmail.com>
2024-03-14 03:47:24 -0700

    Created a world map using chart.js and adjusted the styling

---
+++ comp4945-aspnetcore-app/wwwroot/js/index.js
@@ -0,0 +1,44 @@
+const url = "https://unpkg.com/world-atlas@2.0.2/countries-50m.json";
+fetch(url)
+    .then((result) => result.json())
+    .then((datapoint) => {
+        const countries = ChartGeo.topojson.feature(
+            datapoint,
+            datapoint.objects.countries
+        ).features;
+
+        const data = {
+            labels: countries.map(country => country.properties.name),
+            datasets: [{
+                label: 'Countries',
+                data: countries.map(country => ({
+                    feature: country,
+                    value: Math.random()
+                })),
+            }]
+        };
+
+        // config
+        const config = {
+            type: 'choropleth',
+            data,
+            options: {
+                scales: {
+                    projection: {
+                        axis: 'x',
+                        projection: 'equalEarth',
+                    },
+                },
+                plugins: {
+                    legend: {
+                        display: false,
+                    }
+                }
+            }
+        };
+        // render init block
+        const myChart = new Chart(
+            document.getElementById("myChart"),
+            config
+        );
+    });
